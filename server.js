4463c7eb97956157df9b979ff05a50847a8d37df
dev34@dev34-All-Series-ssd <nedovicmilija@gmail.com>
2018-07-30 10:54:07 +0200

    d

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.PORT || 8080);
 
 
 
 app.get('/*', function(req, res) {
 res.sendFile(path.join(__dirname + '/dist/restoran-app/index.html'));
 })
+app.listen(process.env.PORT || 8080);
+
 
 console.log('Console listening');
