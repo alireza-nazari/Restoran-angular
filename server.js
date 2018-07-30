e5104663df26c48b2d982535ab1b4b18cae1fd1d
dev34@dev34-All-Series-ssd <nedovicmilija@gmail.com>
2018-07-30 11:09:12 +0200

    commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/restoran-app'));
 
 
 
@@ -11,6 +11,5 @@ app.get('/*', function(req, res) {
 res.sendFile(path.join(__dirname + '/dist/restoran-app/index.html'));
 })
 app.listen(process.env.PORT || 8080);
-
 
 console.log('Console listening');
