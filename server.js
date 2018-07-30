77247a7cd19f8433f474af4dcb2b0e4de3cab10d
dev34@dev34-All-Series-ssd <nedovicmilija@gmail.com>
2018-07-30 09:51:40 +0200

    herokui

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080);
 
-//Path Location Strategy
+
 
 app.get('/*', function(req, res) {
 res.sendFile(path.join(__dirname + '/dist/index.html'));
 })
+
+console.log('Console listening');
